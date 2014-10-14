bba43cbe43a7e80bb9f8b9cf642038cb9f903b1c
fabio <fabio.casati@gmail.com>
2014-10-14 14:50:07 +0200

    add club type

--- data/club.py
+++ data/club.py
@@ -12,13 +12,21 @@ class Club(ndb.Model):
     url = ndb.StringProperty(required=True)
 
     owners = ndb.JsonProperty(required=True, indexed=True) # or repeated string?
+    language = ndb.StringProperty(required=True)
+    training_type = ndb.JsonProperty(required=True, indexed=True) # stability, balance,...
 
     is_open = ndb.BooleanProperty(default=True)
-    tags =  ndb.JsonProperty(required=True, indexed=True)
-    members = ndb.JsonProperty(required=True, indexed=True)
-    courses = ndb.JsonProperty(required=True)
-    
+    tags = ndb.JsonProperty(required=True, indexed=True)
+    members = ndb.JsonProperty(required=True, indexed=True) # list of members id
+    courses = ndb.JsonProperty(required=True) # list of dictionaries?
+
+
+
     @staticmethod
     def get_all_clubs():
         return Club.query()
+
+
+
+
 
